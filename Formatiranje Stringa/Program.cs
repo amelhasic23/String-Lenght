44ec5c8504e33956c92f303225c0d1a3d1a71fc2
amelhasic23 <117470943+amelhasic23@users.noreply.github.com>
2024-11-28 09:28:56 +0100

    Commit

---
+++ Formatiranje Stringa/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string firstName = "Peter";
+        string lastName = "Jackson";
+        string birthDate = "01/01/1980";
+
+        string userInfo = $"First name: {firstName}, Last name: {lastName}, Birth date: {birthDate}";
+        Console.WriteLine(userInfo);
+    }
+}
